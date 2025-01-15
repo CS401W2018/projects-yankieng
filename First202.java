228f5ab9349ae24c013f1950e3e81deaf0cb0a59
Yankie Ng <yankieng@go.byuh.edu>
2025-01-15 17:08:53 +0800

    Add Activities

---
+++ First202.java
@@ -0,0 +1,11 @@
+public class First202 {
+
+
+    public static void main(String[] args) {
+        var window = new JFrame();
+        window.setSize(200,200);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new First202());
+        window.setVisible(true);
+    }
+}
